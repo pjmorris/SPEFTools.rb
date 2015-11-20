21fc5d348ac0e9aed7e6decf0538c16c8e5315c2
Patrick Morrison <pmorrisonfl@gmail.com>
2015-11-20 15:48:46 -0500

    same headers for issues, emails, refactored keywords into their own module

--- extract_email.rb
+++ extract_email.rb
@@ -1,35 +1,7 @@
 require 'rubygems'
 require 'json'
 require 'Time'
-
-
-# Keywords to match against our security, practice topics
-term1 = /crash|denial of service|access level|sizing issues|resource consumption|data loss|flood|integrity|overflow|null problem|overload|protection|leak/
-term2 = /security|vulnerability|vulnerable|hole|exploit|attack|bypass|backdoor|threat|expose|breach|violate|fatal|blacklist|overrun|insecure/
-
-ACS = /street address|credit card number|data classification|data inventory|Personally Identifiable Information (PII)|user data|privacy/
-ASR = /authentication|authorization|requirement|use case|scenario|specification|confidentiality|availability|integrity|non-repudiation|user role|regulations|contractual agreements|obligations|risk assessment|FFIEC|GLBA|OCC|PCI DSS|SOX|HIPAA/ 
-PTM = /threats|attackers|attacks|attack pattern|attack surface|vulnerability|exploit|misuse case|abuse case/ 
-DTS = /stack|operating system|database|application server|runtime environment|language|library|component|patch|framework|sandbox|environment|network|tool|compiler|service|version/ 
-
-Topics = [
-         { :topic => "PMASA?", :keywords => /PMASA|home_page\/security/ },
-         { :topic => "Security1", :keywords => term1 },
-         { :topic => "Security2", :keywords => term2 },
-         { :topic => "Apply Classification Scheme", :keywords => ACS },
-         { :topic => 'Apply Security Requirements', :keywords => ASR },
-	 { :topic => 'Perform Threat Modeling', :keywords => PTM },
-	 { :topic => 'Document Technical Stack', :keywords => DTS },
-{ :topic => 'Apply Secure Coding Standards', :keywords =>	/avoid|banned|buffer overflow|checklist|code|code review|code review checklist|coding technique|commit checklist|dependency|design pattern|do not use|enforce function|firewall|grant|input validation|integer overflow|logging|memory allocation|methodology|policy|port|security features|security principle|session|software quality|source code|standard|string concatenation|string handling function|SQL Injection|unsafe functions|validate|XML parser/ },
-{ :topic => 'Apply Security Tooling', :keywords =>	/automate|automated|automating|code analysis|coverage analysis|dynamic analysis|false positive|fuzz test|fuzzer|fuzzing|malicious code detection|scanner|static analysis|tool/ },
-{ :topic => 'Perform Security Testing', :keywords =>	/boundary value|boundary condition|edge case|entry point|input validation|interface|output validation|replay testing|security tests|test|tests|test plan|test suite|validate input|validation testing|regression test/ },
-{ :topic => 'Perform Penetration Testing', :keywords => 	/penetration/ },
-{ :topic => 'Perform Security Review', :keywords =>	/architecture analysis|attack surface|bug bar|code review|denial of service|design review|elevation of privilege|information disclosure|quality gate|release gate|repudiation|review|security design review|security risk assessment|spoofing|tampering|STRIDE/ },
-{ :topic => 'Publish Operations Guide', :keywords =>	/administrator|alert|configuration|deployment|error message|guidance|installation guide|misuse case|operational security guide|operator|security documentation|user|warning/ },
-{ :topic => 'Track Vulnerabilities', :keywords =>	/bug|bug bounty|bug database|bug tracker|defect|defect tracking|incident|incident response|severity|top bug list|vulnerability|vulnerability tracking/ },
-{ :topic => 'Improve Development Process', :keywords =>	/architecture analysis|code review|design review|development phase,gate|root cause analysis|software development lifecycle|software process/ },
-{ :topic => 'Provide Training', :keywords =>	/awareness program|class|conference|course|curriculum|education|hiring|refresher|mentor|new developer|new hire|on boarding|teacher|training/ } 
-         ]
+require './SPEF_Keywords'
  
 # print line with project, date, source, creator, issue #, reporter, keywords, topic
 def write_data_row(d_id,project_month,d_created_at,d_reportedBy,d_owner,project="phpMyAdmin",topic="",source="Bug Tracker",count,filename,d_content)
@@ -38,9 +10,9 @@ def write_data_row(d_id,project_month,d_created_at,d_reportedBy,d_owner,project=
   d_reportedBy, ", ", d_owner, ", " , "\n" # d_content, ", ", count, ", ", filename
 end
 
-pathname = ARGV[0]
-project = ARGV[1]
-print "ProjectMonth,EventDate,Project,Practice,Source,DocId,creator,assignee,\n"
+project = ARGV[0]
+pathname = ARGV[1]
+# print "ProjectMonth,EventDate,Project,Practice,Source,DocId,creator,assignee\n"
 Dir.foreach(pathname) do |filename|
 next if filename == '.' or filename == '..'
 count = 0
@@ -54,7 +26,7 @@ File.open(pathname + '/' + filename).each do |l|
   end
   if match = l[/^From (.*)$/,1]
     if count > 0
-      Topics.map { |t| 
+      SPEF_Keywords::Topics.map { |t| 
         if d_content.scan(t[:keywords]).length > 0
           write_data_row(d_id,project_month,d_created_at,d_reportedBy,d_owner,project=project,t[:topic],source="email",count,filename,d_content)
         end
@@ -73,12 +45,12 @@ File.open(pathname + '/' + filename).each do |l|
       # puts "d_id: ", d_id
     end
     if match  = l[/^Date: (.*)/, 1]
-      d_created_at = match
+      tmp_date = match
       begin
-        d_created_at = (Date.parse d_created_at).strftime("%Y-%m-%d")
-        project_month = ((Date.parse d_created_at) >> 1).strftime("%Y-%m-01")
+        d_created_at = (Date.parse tmp_date).strftime("%Y-%m-%d")
+        project_month = ((Date.parse tmp_date) >> 1).strftime("%Y-%m-01")
       rescue ArgumentError
-        d_created_at = project_month
+        # d_created_at = save_date
       end
       # puts "project_month: ", d_created_at
     end
--- extract_issue.rb
+++ extract_issue.rb
@@ -12,47 +12,6 @@ def get_content(issue)
 return contents
 end
 
-module SPEF_Keywords
-    # Keywords to match against our security, practice topics
-    williams_security_terms = /crash|denial of service|access level|sizing issues|resource consumption|data loss|flood|integrity|overflow|null problem|overload|protection|leak/
-    gegick_security_terms = /security|vulnerability|vulnerable|hole|exploit|attack|bypass|backdoor|threat|expose|breach|violate|fatal|blacklist|overrun|insecure/
-
-   ADCS = /street address|credit card number|data classification|data inventory|Personally Identifiable Information (PII)|user data|privacy/
-   ASR = /authentication|authorization|requirement|use case|scenario|specification|confidentiality|availability|integrity|non-repudiation|user role|regulations|contractual agreements|obligations|risk assessment|FFIEC|GLBA|OCC|PCI DSS|SOX|HIPAA/ 
-   PTM = /threats|attackers|attacks|attack pattern|attack surface|vulnerability|exploit|misuse case|abuse case/ 
-   DTS = /stack|operating system|database|application server|runtime environment|language|library|component|patch|framework|sandbox|environment|network|tool|compiler|service|version/ 
-
-   ASCS =	/avoid|banned|buffer overflow|checklist|code|code review|code review checklist|coding technique|commit checklist|dependency|design pattern|do not use|enforce function|firewall|grant|input validation|integer overflow|logging|memory allocation|methodology|policy|port|security features|security principle|session|software quality|source code|standard|string concatenation|string handling function|SQL Injection|unsafe functions|validate|XML parser/ 
-
-   AST = /automate|automated|automating|code analysis|coverage analysis|dynamic analysis|false positive|fuzz test|fuzzer|fuzzing|malicious code detection|scanner|static analysis|tool/ 
-   PST = /boundary value|boundary condition|edge case|entry point|input validation|interface|output validation|replay testing|security tests|test|tests|test plan|test suite|validate input|validation testing|regression test/ 
-   PPT = /penetration/ 
-   PSR = /architecture analysis|attack surface|bug bar|code review|denial of service|design review|elevation of privilege|information disclosure|quality gate|release gate|repudiation|review|security design review|security risk assessment|spoofing|tampering|STRIDE/ 
-
-   POG = /administrator|alert|configuration|deployment|error message|guidance|installation guide|misuse case|operational security guide|operator|security documentation|user|warning/ 
-   TV = /bug|bug bounty|bug database|bug tracker|defect|defect tracking|incident|incident response|severity|top bug list|vulnerability|vulnerability tracking/ 
-   IDP = /architecture analysis|code review|design review|development phase,gate|root cause analysis|software development lifecycle|software process/ 
-   PSTR = /awareness program|class|conference|course|curriculum|education|hiring|refresher|mentor|new developer|new hire|on boarding|teacher|training/ 
-
-   Topics = [
-         { :topic => "PMASA", :keywords => /PMASA|home_page\/security/ },
-         { :topic => "Security Related", :keywords => williams_security_terms },
-         { :topic => "Security Related", :keywords => gegick_security_terms },
-         { :topic => "Apply Data Classification Scheme", :keywords => ADCS },
-         { :topic => 'Apply Security Requirements', :keywords => ASR },
-	 { :topic => 'Perform Threat Modeling', :keywords => PTM },
-	 { :topic => 'Document Technical Stack', :keywords => DTS },
-         { :topic => 'Apply Secure Coding Standards', :keywords => ASCS },
-         { :topic => 'Apply Security Tooling', :keywords =>	AST },
-         { :topic => 'Perform Security Testing', :keywords =>	PST },
-         { :topic => 'Perform Penetration Testing', :keywords => 	PPT },
-         { :topic => 'Perform Security Review', :keywords => PSR },
-         { :topic => 'Publish Operations Guide', :keywords => POG },
-         { :topic => 'Track Vulnerabilities', :keywords => TV },	
-         { :topic => 'Improve Development Process', :keywords => IDP },
-         { :topic => 'Provide Security Training', :keywords =>	PSTR }
-         ]
-end # module SPEF_Keywords
 
 require './SPEF_Keywords'
  
